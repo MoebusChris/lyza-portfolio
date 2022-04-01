84849b644a9a4c1fc69194fcbf29353ef97ac664
Subeom Jeong <subeomjeong@Subeoms-MBP.attlocal.net>
2022-03-31 21:37:37 -0700

    initial commit

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About () {
+    return (
+        <section>
+            <h1 id="about">Polar is not just a dog, She's my family.</h1>
+        </section>
+    );
+}
+
+export default About;
